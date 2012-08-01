c899e5aa34d759eb4736f4f6cbf43a6f4e698a9e
Charles Feduke <charles.feduke@gmail.com>
2012-08-01 11:59:14 -0400

    a bit more resiliency

--- lib/item.rb
+++ lib/item.rb
@@ -2,15 +2,23 @@ class Item
   attr_reader :title, :link, :description, :html
 
   def initialize(raw)
+    @raw = raw
     @title = raw.at(:title).html
-    @link = raw.at('link').next # bogus yahoo RSS XML
-    @description = raw.at(:description)
-    @html = raw.at('media:text')
-    @html = @html.html if @html.respond_to? :html
+    @link = html_content_at :link
+    # handle bogus yahoo RSS XML
+    @link = raw.at('link').next if @link.nil? || @link.empty?
+    @description = html_content_at :description
+    @html = html_content_at 'media:text'
     @description = @html unless @html.nil?
   end
 
   def to_s
     "#{@title} - #{@link}\n#{@description}\n\n"
+  end
+
+  private
+  def html_content_at(location)
+    value = @raw.at(location)
+    value = value.html if value.respond_to? :html
   end
 end
